5469a12ba40cc96558d744dfc9d48b8323e0d3d1
Mbornoe <Mbornoe@gmail.com>
2012-11-20 10:01:01 +0100

    d

--- yapstr/CameraViewController.h
+++ yapstr/CameraViewController.h
@@ -11,11 +11,34 @@
  */
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
 
-@interface CameraViewController : UIViewController
+@interface CameraViewController : UIViewController<UINavigationControllerDelegate,UIImagePickerControllerDelegate>{
+    UIImagePickerController*imagePicker;
+    
+}
+
+- (IBAction)showCameraUI:(id)sender;
+- (IBAction)uploadPhoto:(id)sender;
 
 - (void) startCamera;
 - (void) takePhoto;
 - (void) uploadPhoto;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+// Transform values for full screen support:
+#define CAMERA_TRANSFORM_X 1
+//#define CAMERA_TRANSFORM_Y 1.12412 // this was for iOS 3.x
+#define CAMERA_TRANSFORM_Y 1.24299 // this works for iOS 4.x
+
+// iPhone screen dimensions:
+#define SCREEN_WIDTH  320
+#define SCREEN_HEIGTH 480
+
+//#define SCREEN_WIDTH  640
+//#define SCREEN_HEIGTH 1136
 
 @end
--- yapstr/CameraViewController.m
+++ yapstr/CameraViewController.m
@@ -18,6 +18,51 @@
 
 @implementation CameraViewController
 
+@synthesize imageView;
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    NSLog(@"Hej! Jeg har er viewWillAppear");
+    
+    // shadowPath, shadowOffset, and rotation is handled by ECSlidingViewController.
+    // You just need to set the opacity, radius, and color.
+    self.view.layer.shadowOpacity = 0.75f;
+    self.view.layer.shadowRadius = 10.0f;
+    self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+    
+    if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
+        self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    }
+    [self.view addGestureRecognizer:self.slidingViewController.panGesture];
+    
+    
+    
+}
+
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+    [self startCameraControllerFromViewController: self
+                                    usingDelegate: self];
+    
+
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    UIImage *image;
+    image =(UIImage *)[info valueForKey:UIImagePickerControllerOriginalImage];
+    imageView.image=image;
+    UIImage *imageToSave;
+    imageToSave=imageView.image;
+    UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil);
+    [picker dismissViewControllerAnimated:YES completion:nil];
+        
+}
+
+    
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -26,9 +71,16 @@
     }
     return self;
 }
+    
+    
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+        [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 - (void)viewDidLoad
 {
+    imagePicker=[[UIImagePickerController alloc]init];
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
@@ -38,6 +90,66 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
+
+                                   usingDelegate: (id <UIImagePickerControllerDelegate,
+                                                   
+                                                   UINavigationControllerDelegate>) delegate {
+    
+    
+    
+    if (([UIImagePickerController isSourceTypeAvailable:
+          
+          UIImagePickerControllerSourceTypeCamera] == NO)
+        
+        || (delegate == nil)
+        
+        || (controller == nil))
+        
+        return NO;
+    
+    
+    
+    
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    
+    
+    // Displays a control that allows the user to choose picture or
+    
+    // movie capture, if both are available:
+    
+    //cameraUI.mediaTypes =[UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    
+    
+    
+    // Hides the controls for moving & scaling pictures, or for
+    
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.showsCameraControls = YES;
+	cameraUI.navigationBarHidden = YES;
+    cameraUI.allowsEditing = NO;
+    
+    // Make camera view full screen:
+	cameraUI.wantsFullScreenLayout = YES;
+	cameraUI.cameraViewTransform = CGAffineTransformScale(cameraUI.cameraViewTransform, CAMERA_TRANSFORM_X, CAMERA_TRANSFORM_Y);
+    
+    cameraUI.delegate = delegate;
+    
+    //  cameraUI.cameraOverlayView = ;
+    
+    [controller presentModalViewController: cameraUI animated: YES];
+    //[self imagePickerController: self
+      //                              usingDelegate: self];
+    
+    return YES;
+    
+}
+
 
 - (void) startCamera
 {
@@ -50,6 +162,9 @@
 - (void) uploadPhoto
 {
     
+}
+- (IBAction)revealSideMenu:(id)sender {
+    [self.slidingViewController anchorTopViewTo:ECRight];
 }
 
 @end
