a3fabdc635f736ddcd1368cb63b47d3c9004180a
Jonas Markussen <jonas@h66.net2.klient.njv14.site.aau.dk>
2012-11-20 10:58:04 +0100

    removed shadows on menu to increase performance

--- yapstr/EventViewController.m
+++ yapstr/EventViewController.m
@@ -73,9 +73,9 @@
 
     // shadowPath, shadowOffset, and rotation is handled by ECSlidingViewController.
     // You just need to set the opacity, radius, and color.
-    self.view.layer.shadowOpacity = 0.75f;
-    self.view.layer.shadowRadius = 10.0f;
-    self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+    //self.view.layer.shadowOpacity = 0.75f;
+    //self.view.layer.shadowRadius = 10.0f;
+    //self.view.layer.shadowColor = [UIColor blackColor].CGColor;
 
     if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
         self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
