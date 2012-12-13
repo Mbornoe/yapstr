745f20e0472a61819de212ff9dde495abb4d1d00
Kildahl <Kildahl@h32.net2.klient.njv14.site.aau.dk>
2012-12-13 10:52:10 +0100

    SelectPhoto and LoginController fixt

--- yapstr/LoginViewController.h
+++ yapstr/LoginViewController.h
@@ -25,14 +25,8 @@
 /** Reference to an external String that are used by Facebook. */
 extern NSString *const FacebookDataLoadedNotification;
 
-/** Reference to the a bool if the user is logged in or not. */
-@property BOOL* logout;
-
 /** Reference to an instance of FacebookUser from the Facebook Model. */
 @property (strong, nonatomic) FacebookUser* myFacebook;
-
-/** Reference to the a bool if the facebookdata is loaded or not. */
-@property (nonatomic, assign) BOOL facebookDataLoaded;
 
 /** Reference to the ActivityIndicatorView that are used to indicate that the method is currently loading.  */
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *loading;
@@ -41,11 +35,7 @@ extern NSString *const FacebookDataLoadedNotification;
 @property (strong, nonatomic) IBOutlet UIButton *loginButton;
 
 /** Opens a Facebook session using the framework FacebookSDK. */
-- (BOOL)openSessionWithAllowLoginUI:(BOOL)allowLoginUI;
-
-
-/** Method that will close the Facebook session and define the related attributes to nil. */
-- (void)logFacebookOut;
+- (BOOL)sendLoginInfoToFacebook:(BOOL)allowLoginUI;
 
 /** This method collects data about the user. The data that are pulled is facebookID and name. This information should be sent to the external server to see if the user has been using the application before. */
 - (void)collectUserData;
